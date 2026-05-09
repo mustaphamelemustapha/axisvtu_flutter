1fbb5f8772bef212ac38489265d81e4a649b512d
Mustapha Mele Mustapha <mustaphamelemustapha@gmail.com>
2026-05-09 08:13:44 +0100

    chore: bump version and migrate Android theme to MaterialComponents with updated dependencies

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -67,3 +67,8 @@ android {
 flutter {
     source = "../.."
 }
+
+dependencies {
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.9.0")
+}
